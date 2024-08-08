140c3ba74b4219d69e3b96450b76b7c9ed58dc6c
Justin Allard <justinallard@icloud.com>
2024-08-08 16:01:22 -0500

    Update WPBlog.js

--- src/components/Blog/WPBlog.js
+++ src/components/Blog/WPBlog.js
@@ -12,9 +12,9 @@ export default function WPBlog({ post }) {
   <div class="project-card-view card">
     <div class="card-body">
       <div class="card-title h5">{post.title.rendered}</div>
-      <p class="card-text" style="text-align: justify;">{{ __html: post.excerpt.rendered }}</p>
+      <p class="card-text" style="text-align: justify;">{post.excerpt.rendered}</p>
       
-      <a role="button" tabindex="0" href="" class="btn btn-primary">Read More</a>
+      <a role="button" tabindex="0" href="/" class="btn btn-primary">Read More</a>
 
 </div>
 </div>
