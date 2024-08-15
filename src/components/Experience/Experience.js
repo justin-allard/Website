518eef6e6ad1542363dde81825e8de4e593ed874
Justin Allard <justinallard@icloud.com>
2024-08-15 13:31:07 -0500

    Update Experience.js

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,4 +1,4 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Techstack from "./Techstack";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -19,13 +19,109 @@ function Experience() {
         </Row>
 
         <Row>
-        
-        <h3>Heartland Business Systems</h3>
+
+        <Col xs={12} md={4}>
+
+      </Col>
+      <Col xs={12} md={8}>
+      <h3>Heartland Business Systems</h3>
         
    
-   <p>November 2016 – Present<br/>Software Engineer<br/><br/>I develop and maintain websites primarily using Kentico Experience and the .net stack. I also have used many other CMS tools including WordPress and Sitecore.</p>
-   
+        <p>November 2016 – Present<br/>Software Engineer<br/><br/>I develop and maintain websites primarily using Kentico Experience and the .net stack. I also have used many other CMS tools including WordPress and Sitecore.</p>
+        
+</Col>
+        
+        
         </Row>
+
+        <Row>
+
+<Col xs={12} md={4}>
+
+</Col>
+<Col xs={12} md={8}>
+<h3>Heartland Business Systems</h3>
+
+
+<p>November 2016 – Present<br/>Software Engineer<br/><br/>I develop and maintain websites primarily using Kentico Experience and the .net stack. I also have used many other CMS tools including WordPress and Sitecore.</p>
+
+</Col>
+
+
+</Row>
+
+<Row>
+
+<Col xs={12} md={4}>
+
+</Col>
+<Col xs={12} md={8}>
+<h3>UW-Green Bay</h3>
+
+
+</Col>
+
+
+</Row>
+
+
+<Row>
+
+<Col xs={12} md={4}>
+
+</Col>
+<Col xs={12} md={8}>
+<h3>Free Lance Web Developer</h3>
+
+
+</Col>
+
+
+</Row>
+
+
+<Row>
+
+<Col xs={12} md={4}>
+
+</Col>
+<Col xs={12} md={8}>
+<h3>Adams-Columbia Electric Cooperative</h3>
+
+
+</Col>
+
+
+</Row>
+
+
+<Row>
+
+<Col xs={12} md={4}>
+
+</Col>
+<Col xs={12} md={8}>
+<h3>UW – Green Bay</h3>
+
+
+</Col>
+
+
+</Row>
+
+<Row>
+
+<Col xs={12} md={4}>
+
+</Col>
+<Col xs={12} md={8}>
+<h3>Wisconsin Space Grant Consortium</h3>
+
+
+</Col>
+
+
+</Row>
 
 
         <h2>
