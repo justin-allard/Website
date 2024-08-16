1638e0f3ac65f5369267fa7b9741a97262624662
Justin Allard <justinallard@icloud.com>
2024-08-16 10:32:37 -0500

    Update WPBlogDetail.js

--- src/components/BlogDetail/WPBlogDetail.js
+++ src/components/BlogDetail/WPBlogDetail.js
@@ -1,27 +1,46 @@
 import React from "react";
+import { Container, Row, Col} from "react-bootstrap";
 
 
 export default function WPBlogDetail({ post }) {
 
 
   return (
-  
-<div class="project-card col-md-4">
-  <div class="project-card-view card">
-    <div class="card-body">
-      <div class="card-title blogTitle">{post.title.rendered}</div>
-      
-  
-      <p
+  <div class="blogDetail">
+
+
+<Container>
+<Row>
+        <Col xs={12} md={12}>
+        <h1>{post.title.rendered}</h1>
+      </Col>
+        </Row>
+
+        <Row>
+        <Col xs={12} md={12}>
+        <p
           className="card-text"
           style={{textAlign: 'justify'}}
-          dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+          dangerouslySetInnerHTML={{ __html: post.content.rendered }}
         />
-      
-      <a role="button" tabindex="0" href="/" class="btn btn-primary">Read More</a>
+      </Col>
+        </Row>
 
-</div>
-</div>
+        <Row>
+        <Col xs={12} md={12}>
+        <a role="button" tabindex="0" href="/blog" class="btn btn-primary">More Blogs</a>
+      </Col>
+        </Row>
+
+
+</Container>
+
+
+      
+      
+
+
+
 </div>
 
   );
